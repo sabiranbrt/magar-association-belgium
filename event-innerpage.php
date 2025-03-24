6c3e9e58057e8505b2a7aa3038e849f30e8fbcfc
sabiran shah <sabiran.brt@gmail.com>
2025-03-24 18:52:18 +0545

    merge

---
+++ event-innerpage.php
@@ -0,0 +1,68 @@
+<?php include 'common/header.php'; ?>
+<?php include 'common/navigation.php'; ?>
+
+<!-- banner common for all archieve pages -->
+
+<div class="banner">
+    <div class="image">
+        <div class="banner-wrapper">
+            <img src="assets/image/wall/wall.png" alt="">
+            <div class="image_logo">
+                <img src="assets/image/wall/wall-i.png" alt="">
+            </div>
+            <div class="image_logo">
+                <img src="assets/image/wall/wall-i.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="title">
+        <p>OUR EVENTS</p>
+    </div>
+</div>
+
+<div class="event_inner_page">
+    <div class="container">
+        <div class="wrapper">
+            <div class="title">
+                <h2>Program For ThePreservation & promotion of
+                    <p class="highlight"> Magar Language & culture.</p>
+                </h2>
+            </div>
+            <div class="detail_wrapper">
+                <div class="venue">
+                    <div class="venue_detail">
+                        <img src="assets/icon/dateicon.svg" alt="">
+                        <p class="paragraph-text">Jun 21,2024</p>
+                    </div>
+                    <div class="venue_detail">
+                        <img src="assets/icon/clockicon.svg" alt="">
+                        <p class="paragraph-text">8:00 am - 4:00 pm</p>
+                    </div>
+                    <div class="venue_detail">
+                        <img src="assets/icon/locationicon.svg" alt="">
+                        <p class="paragraph-text">Smart school</p>
+                    </div>
+                </div>
+                <div class="share">
+                    <p>Share On</p>
+                    <div class="social">
+                        <a class="social_sites">
+                            <div class="icons"></div>
+                        </a>
+                        <a class="social_sites">
+                            <div class="icons"></div>
+                        </a>
+                        <a class="social_sites">
+                            <div class="icons"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="img">
+                <img src="assets/image//event/eventw.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'common/footer.php'; ?>
--- index.php
+++ index.php
@@ -11,14 +11,14 @@
         <div class="bg-lt-rt">
             <div class="container">
                 <div class="bg-lt-rt-main">
-                <div class="right_image">
+                    <div class="right_image">
                         <img class="png-img-ii" src="assets/image/wall/wall-i.png" alt="left-wall-staker">
-                        </div>
+                    </div>
                     <div class="bg-lt">
                         <div class="left_image">
-                        <img class="png-img-i" src="assets/image/wall/wall-i.png" alt="left-wall-staker">
+                            <img class="png-img-i" src="assets/image/wall/wall-i.png" alt="left-wall-staker">
                         </div>
-                        
+
                         <div class="bg-header">
                             <h1>A common representative of the <span class="highlight">Magar indigenous</span> people of
                                 Nepal.</h1>
@@ -31,11 +31,11 @@
                         </div>
                         <a class="primary-btn border-transparent">
                             <p>Explore Now</p>
-                            <div class="arrow-btn"></div>      
+                            <div class="arrow-btn"></div>
                         </a>
                     </div>
                     <div class="bg-rt">
-                        
+
                         <div class="bg-main-img">
                             <img src="assets/image/wall/wall-person.png" alt="wall-person">
                         </div>
@@ -61,16 +61,16 @@
                 <div class="regis-txt">
                     <p>
                         Lorem ipsum dolor sit amet consectetur. Blandit fames sceler isq ue auctor vel diam quis. Sed
-                        tortor ultricies blandit cursus risus ipsum libero quis. Quam neque nulla mi integer.   Lorem ipsum dolor sit amet consectetur. Blandit fames sceler isq ue auctor vel diam quis. Sed
+                        tortor ultricies blandit cursus risus ipsum libero quis. Quam neque nulla mi integer. Lorem ipsum dolor sit amet consectetur. Blandit fames sceler isq ue auctor vel diam quis. Sed
                         tortor ultricies blandit cursus risus ipsum libero quis. Quam neque nulla mi integer.
                     </p>
                 </div>
                 <div>
-                <a href="#" class="secondary-btn border-transparent">
-                    <p>Register Now</p>
-                    <div class="arrow-btn"></div>
-                </a>
-                 </div>
+                    <a href="#" class="secondary-btn border-transparent">
+                        <p>Register Now</p>
+                        <div class="arrow-btn"></div>
+                    </a>
+                </div>
             </div>
         </div>
 
@@ -89,10 +89,10 @@
                     </p>
                 </div>
                 <div>
-                <a class="secondary-btn border-transparent">
-                    <p>Register Now</p>
-                    <div class="arrow-btn"></div>
-                </a>
+                    <a class="secondary-btn border-transparent">
+                        <p>Register Now</p>
+                        <div class="arrow-btn"></div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -115,14 +115,14 @@
                 </div>
             </div>
             <div class="about-main-rt">
-            <div class="head_title">
-              <div class="head_image">
-              <img src="assets/image/wall/wall-i.png" alt="steaker">
-              </div>
-    <div class="title">
-        <h3>About Magar Association Belgium</h3>
-    </div>
-         </div>
+                <div class="head_title">
+                    <div class="head_image">
+                        <img src="assets/image/wall/wall-i.png" alt="steaker">
+                    </div>
+                    <div class="title">
+                        <h3>About Magar Association Belgium</h3>
+                    </div>
+                </div>
 
                 <div class="about-main-title">
                     <h1>Preservation & promotion of <span class="highlight">Magar culture</span>.</h1>
@@ -141,7 +141,7 @@
                 </div>
                 <a class="primary-btn">
                     <p>Read More</p>
-                    <div class="arrow-btn"></div>  
+                    <div class="arrow-btn"></div>
                 </a>
             </div>
         </div>
@@ -154,14 +154,14 @@
     <div class="container">
         <div class="chairperson-lt-rt">
             <div class="chairperson-lt">
-            <div class="head_title">
-              <div class="head_image">
-                 <img src="assets/image/wall/wall-i.png" alt="steaker">
-              </div>
-                 <div class="title">
-                     <h3>Message From President</h3>
-                 </div>
-             </div>
+                <div class="head_title">
+                    <div class="head_image">
+                        <img src="assets/image/wall/wall-i.png" alt="steaker">
+                    </div>
+                    <div class="title">
+                        <h3>Message From President</h3>
+                    </div>
+                </div>
                 <div class="about-main-title">
                     <h1>Preservation & promotion of <span class="highlight">Magar culture</span>.</h1>
                 </div>
@@ -197,14 +197,14 @@
 <div class="main-program-container">
     <div class="container">
         <div class="program-content">
-        <div class="head_title justify_center">
-              <div class="head_image">
-                 <img src="assets/image/wall/wall-i.png" alt="steaker">
-              </div>
-                 <div class="title">
-                     <h3>Our Programs & Initiatives</h3>
-                 </div>
-             </div>
+            <div class="head_title justify_center">
+                <div class="head_image">
+                    <img src="assets/image/wall/wall-i.png" alt="steaker">
+                </div>
+                <div class="title">
+                    <h3>Our Programs & Initiatives</h3>
+                </div>
+            </div>
             <div class="about-main-title">
                 <h1>Our Updated <span class="highlight">Program & Initiative</span>.</h1>
             </div>
@@ -336,14 +336,14 @@
     <div class="container">
         <div class="event-lt-rt">
             <div class="event-lt">
-            <div class="head_title">
-              <div class="head_image">
-                 <img src="assets/image/wall/wall-i.png" alt="steaker">
-              </div>
-                 <div class="title">
-                     <h3>Our Events</h3>
-                 </div>
-             </div>
+                <div class="head_title">
+                    <div class="head_image">
+                        <img src="assets/image/wall/wall-i.png" alt="steaker">
+                    </div>
+                    <div class="title">
+                        <h3>Our Events</h3>
+                    </div>
+                </div>
                 <div class="about-main-title">
                     <h1>Join Our Latest New Upcoming Events</h1>
                 </div>
@@ -359,9 +359,9 @@
                         sapiente, inventore quo! Neque, minus.
                     </p>
                 </div>
-                <a class="primary-btn">
+                <a href="event.php" class="primary-btn">
                     <p>View All Events</p>
-                    <div class="arrow-btn"></div>   
+                    <div class="arrow-btn"></div>
                 </a>
             </div>
             <div class="event-rt">
@@ -384,7 +384,7 @@
                         <div class="co-txt">
                             <h3>Learning Exactly how we Arrange a Event</h3>
                         </div>
-                        <a class="primary-btn-text">
+                        <a href="event-innerpage.php" class="primary-btn-text">
                             <p>View Details</p>
                             <div class="arrow-btn"></div>
                         </a>
@@ -414,9 +414,9 @@
                         <div class="co-txt">
                             <h3>Learning Exactly how we Arrange a Event Learning Exactly how we Arrange a Event</h3>
                         </div>
-                        <a class="primary-btn-text">
+                        <a href="event-innerpage.php" class="primary-btn-text">
                             <p>View Details</p>
-                      <div class="arrow-btn"></div>
+                            <div class="arrow-btn"></div>
                         </a>
                     </div>
                     <div class="event-rt-popup">
@@ -434,14 +434,14 @@
 <div class="news-container">
     <div class="container">
         <div class="news-content">
-        <div class="head_title justify_center">
-              <div class="head_image">
-                 <img src="assets/image/wall/wall-i.png" alt="steaker">
-              </div>
-                 <div class="title">
-                     <h3>News & Blogs</h3>
-                 </div>
-             </div>
+            <div class="head_title justify_center">
+                <div class="head_image">
+                    <img src="assets/image/wall/wall-i.png" alt="steaker">
+                </div>
+                <div class="title">
+                    <h3>News & Blogs</h3>
+                </div>
+            </div>
             <div class="about-main-title">
                 <h1>Directly From The Latest <br><span class="highlight">News & Articles</span>.</h1>
             </div>
@@ -467,51 +467,20 @@
                     <div class="readmore-none-wrapper">
                         <div class="txt-icon">
                             <a class="readmore-none-hist">
-                            <p>Read More</p>
-                            <img src="assets/icon/btn-arrow-blue.svg"/>
-                        </a>
-                        </div>
-                        <div>
-                        <a href="">
-                        <div href="" class="sharebtn"></div>
-                        </a></div>
-                    </div>
-                </div>
-                <div class="program-r1-hist hist">
-                    <div class="r1-img">
-                    <div class="news_date">
-                            <p>25 Nov, 2025</p>
-                        </div>
-                        <img src="assets/image/program/program.png" alt="program3">
-                    </div>
-                    <div class="program-r1-txt-wrapper">
-                        <div class="program-r1-title-hist">
-                            <h2>Helping The Homeless During The Hopeless</h2>
-                        </div>
-                        <div class="program-r1-txt-hist">
-                            <p>Lorem ipsum dolor sit amet consectetur. Vitae amet ultricies in sed nibh ipsum vitae.
-                                Egestas
-                                donec.</p>
-                        </div>
-                    </div>
-
-                    <div class="readmore-none-wrapper">
-                        <div class="txt-icon">
-                            <a class="readmore-none-hist">
-                            <p>Read More</p>
-                            <img src="assets/icon/btn-arrow-blue.svg"/>
-                        </a>
+                                <p>Read More</p>
+                                <img src="assets/icon/btn-arrow-blue.svg" />
+                            </a>
                         </div>
                         <div>
                             <a href="">
-                        <div href="" class="sharebtn"></div>
-                        </a>
-                            </div>
+                                <div href="" class="sharebtn"></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div class="program-r1-hist hist">
                     <div class="r1-img">
-                    <div class="news_date">
+                        <div class="news_date">
                             <p>25 Nov, 2025</p>
                         </div>
                         <img src="assets/image/program/program.png" alt="program3">
@@ -530,21 +499,53 @@
                     <div class="readmore-none-wrapper">
                         <div class="txt-icon">
                             <a class="readmore-none-hist">
-                              <p>Read More</p>
-                              <img src="assets/icon/btn-arrow-blue.svg"/>
+                                <p>Read More</p>
+                                <img src="assets/icon/btn-arrow-blue.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <div href="" class="sharebtn"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="program-r1-hist hist">
+                    <div class="r1-img">
+                        <div class="news_date">
+                            <p>25 Nov, 2025</p>
+                        </div>
+                        <img src="assets/image/program/program.png" alt="program3">
+                    </div>
+                    <div class="program-r1-txt-wrapper">
+                        <div class="program-r1-title-hist">
+                            <h2>Helping The Homeless During The Hopeless</h2>
+                        </div>
+                        <div class="program-r1-txt-hist">
+                            <p>Lorem ipsum dolor sit amet consectetur. Vitae amet ultricies in sed nibh ipsum vitae.
+                                Egestas
+                                donec.</p>
+                        </div>
+                    </div>
+
+                    <div class="readmore-none-wrapper">
+                        <div class="txt-icon">
+                            <a class="readmore-none-hist">
+                                <p>Read More</p>
+                                <img src="assets/icon/btn-arrow-blue.svg" />
                             </a>
                         </div>
                         <a>
-                        <div href="" class="sharebtn"></div>
-                            </a>
+                            <div href="" class="sharebtn"></div>
+                        </a>
                     </div>
                 </div>
             </div>
             <div class="bg-btn-centeralign">
                 <a class="primary-btn">
                     <p>View All Program</p>
-                    <div class="arrow-btn"></div> 
-               </a>
+                    <div class="arrow-btn"></div>
+                </a>
             </div>
         </div>
     </div>
